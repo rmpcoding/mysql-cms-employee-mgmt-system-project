cf1d68c93708fe41bf199d0a256dac12b226e006
Robert Parsons <rmpcoding@gmail.com>
2020-05-12 01:06:08 -0700

    Adds seedDatabase() functions

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ connection.connect((err) => {
         return;
     }
     console.log(`connected as id ${connection.threadId}`);
+    // seedDatabase();
     prompt();
 });
 
@@ -52,11 +53,9 @@ const prompt = () => {
                     break;
 
                 case 'View all Employees by Department':
-
                     break;
 
                 case 'View all Employees by Manager':
-
                     break;
 
                 case 'Add Employee':
@@ -68,19 +67,16 @@ const prompt = () => {
                     break;
 
                 case 'Add Department':
-
+                    addDepartment();
                     break;
 
                 case 'Remove Employee':
-
                     break;
 
                 case 'Update Employee Role':
-
                     break;
 
                 case 'Update Employee Manager':
-
                     break;
 
                 case 'View all Roles':
@@ -92,7 +88,6 @@ const prompt = () => {
                     break;
 
                 case 'Terminate':
-
                     break;
             }
         });
@@ -120,18 +115,8 @@ const viewAllDepartments = () => {
     });
 };
 
-// const viewAllEmployeesByDept = () => {connection.query('', (err, results, field) => {
-//     if (err) throw err;
-//     console.table(results);
-// }
-// )};
-
-// const viewAllEmployeesByMngr = () => {connection.query('', (err, results, field) => {
-//     if (err) throw err;
-//     console.table(results);
-// }
-// )};
-
+// ADD EMPLOYEE
+// =========================================================================================
 const addEmployee = () => {
     let questions = [
         {
@@ -154,18 +139,70 @@ const addEmployee = () => {
         },
     ];
 
-    inquirer.prompt(questions).then((answers) => {
+    let questionsForFirstTimeUser = [
+        {
+            name: 'firstName',
+            type: 'input',
+            message: `What's the employee's first name?`,
+            validate: validateLetters,
+        },
+        {
+            name: 'lastName',
+            type: 'input',
+            message: `What's the employee's last name?`,
+            validate: validateLetters,
+        },
+        {
+            name: 'role',
+            type: 'input',
+            message: `What's the employee's role?`,
+        },
+        {
+            name: 'salary',
+            type: 'input',
+            message: `What's the employee's salary associated with this role?`,
+        },
+    ];
+
+    // If the database already has employees, use global employee array for their choice
+    if (roleArr) {
+        inquirer.prompt(questions).then((answers) => {
+            connection.query(
+                `INSERT INTO employee 
+                 VALUES (DEFAULT, '${answers.firstName}', '${answers.lastName}', DEFAULT, DEFAULT);`,
+                (err, results, field) => {
+                    if (err) throw err;
+                    employeeArr.push(
+                        `${answers.firstName} ${answers.lastName}`
+                    );
+                    console.table(results);
+                    console.log(employeeArr);
+                }
+            );
+        });
+    }
+
+    // If there is nothing in the global employee array (first time user), then use this
+    inquirer.prompt(questionsForFirstTimeUser).then((answers) => {
         connection.query(
             `INSERT INTO employee 
-             VALUES (DEFAULT, '${answers.firstName}', '${answers.lastName}', DEFAULT, DEFAULT);`,
+             VALUES (DEFAULT, '${answers.firstName}', '${answers.lastName}', DEFAULT, DEFAULT);
+             INSERT INTO role 
+             VALUES (DEFAULT, '${answers.role}', '${answers.salary}', DEFAULT);`,
             (err, results, field) => {
                 if (err) throw err;
+                employeeArr.push(`${answers.firstName} ${answers.lastName}`);
+                roleArr.push(answers.role);
                 console.table(results);
+                console.log(employeeArr);
+                console.log(roleArr);
             }
         );
     });
 };
 
+// ADD ROLE
+// =========================================================================================
 const addRole = () => {
     let questions = [
         {
@@ -197,8 +234,65 @@ const addRole = () => {
     });
 };
 
-// // connection.end();
+// ADD DEPARTMENT
+// =========================================================================================
 
+const addDepartment = () => {
+    let questions = [
+        {
+            name: 'department',
+            type: 'input',
+            message: `What department would you like to add?`,
+        },
+    ];
+
+    inquirer.prompt(questions).then((answers) => {
+        connection.query(
+            `INSERT INTO department 
+             VALUES (DEFAULT, '${answers.department}')`,
+            (err, results, field) => {
+                if (err) throw err;
+                departmentArr.push(answers.department);
+                console.table(results);
+                console.log(departmentArr);
+            }
+        );
+    });
+};
+
+// REMOVE EMPLOYEES
+// =========================================================================================
+const removeEmployee = () => {
+    if (employeeArr) {
+        let questions = [
+            {
+                name: 'remove',
+                type: 'list',
+                message: `Which employee would you like to remove?`,
+                choices: employeeArr,
+            },
+        ];
+
+        inquirer.prompt(questions).then((answers) => {
+            connection.query(
+                `DELETE FROM employee
+                 WHERE  `,
+                (err, results, field) => {
+                    if (err) throw err;
+                    // update global employee array to reflect updated changes
+                    // invoke slice method? perhaps val.match method? then slice?
+                    console.table(results);
+                    console.log(employeeArr);
+                }
+            );
+        });
+    }
+
+    console.log('Please add an employee before you try to remove it');
+};
+
+// VALIDATION FUNCTIONS
+// =========================================================================================
 const validateLetters = (name) => {
     let pass = name.match(/^[A-Za-z]+$/);
     if (pass) {
@@ -213,4 +307,45 @@ const validateSalary = (salary) => {
         return true;
     }
     return 'Please do not enter any commas.';
+};
+
+// // connection.end();
+
+const seedDatabase = () => {
+    let seedEmployees = 'SELECT first_name, last_name FROM employee';
+    let seedRoles = 'SELECT title FROM role';
+    let seedSalaries = 'SELECT salary FROM role';
+    let seedDepartments = 'SELECT name FROM department';
+
+    connection.query(seedEmployees, (err, res) => {
+        if (err) throw err;
+
+        for (var i = 0; i < res.length; i++) {
+            employeeArr.push(res[i].first_name + ' ' + res[i].last_name);
+        }
+    });
+
+    connection.query(seedRoles, (err, res) => {
+        if (err) throw err;
+
+        for (var i = 0; i < res.length; i++) {
+            roleArr.push(res[i].title);
+        }
+    });
+
+    connection.query(seedSalaries, (err, res) => {
+        if (err) throw err;
+
+        for (var i = 0; i < res.length; i++) {
+            salaryArr.push(res[i].salary);
+        }
+    });
+
+    connection.query(seedDepartments, (err, res) => {
+        if (err) throw err;
+
+        for (var i = 0; i < res.length; i++) {
+            departmentArr.push(res[i].name);
+        }
+    });
 };
